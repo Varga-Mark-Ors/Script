8b29ad45d21630a1564b65cda5c70cbead103f20
Varga-Mark-Ors <90794458+Varga-Mark-Ors@users.noreply.github.com>
2023-09-12 14:51:10 +0200

    Delete 9.12 hazik directory

--- 9.12 hazik/ketszamosszege.py
+++
@@ -1,7 +0,0 @@
-
-def main():
-    a=int(input("Elso szam="))
-    b=int(input("Masodik szam="))
-    print(a+b)
-if __name__=="__main__":
-    main()
--- 9.12 hazik/szamjegyekszama.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-def main():
-    a=2**256
-    b=str(a)
-    print(len(b))
-if __name__=="__main__":
-    main()
